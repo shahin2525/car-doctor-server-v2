001e914b8c160342e90c0f0f38632b5a847215e6
shahin2525 <shahin451999@gmail.com>
2024-11-16 18:36:36 -0800

    add jwt

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongodb from "mongodb";
+import jwt from "jsonwebtoken";
 // const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = mongodb;
 dotenv.config();
@@ -11,7 +12,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 console.log(process.env.DB_PASS);
@@ -36,7 +42,15 @@ async function run() {
 
     const serviceCollection = client.db("carDoctor").collection("services");
     const bookingCollection = client.db("carDoctor").collection("bookings");
-
+    // auth related api
+    app.post("/jwt", async (req, res) => {
+      const data = req.body;
+      const token = jwt.sign(data, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token: token });
+    });
+    // service related api
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
